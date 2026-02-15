3b1e4742952f380c3917af8b3ebfb75313f8542a
GrimbleMori <grimblemori@gmail.com>
2026-02-15 09:23:24 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from constants.py import *
+import pygame
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(F"Screen width: {screen_width}")
+    print(F"Screen height: {screen_height}")
+
+
+if __name__ == "__main__":
+    main()
