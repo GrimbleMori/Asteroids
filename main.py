4d411b02000f9a3684dfd218c770443c1979c2b4
GrimbleMori <grimblemori@gmail.com>
2026-03-15 12:36:52 -0500

    Implemented player sprite into game.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from constants import *
+import player as pl
 from logger import log_state
 import pygame
+
 
 def main():
     pygame.init()
@@ -10,14 +12,17 @@ def main():
     print(F"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    player = pl.Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
