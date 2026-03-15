10029d07714258dd3fb8f88ab0ac3889844acb11
GrimbleMori <grimblemori@gmail.com>
2026-03-15 14:25:42 -0500

    Added pygame groups for objects.

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     print(F"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    pl.Player.containers = (updatable, drawable)
     player = pl.Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         log_state()
@@ -19,8 +22,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
